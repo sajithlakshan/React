e1449e963ad52fed49e4d565bf02524e3b5e966b
sajithlakshan <57319180+sajithlakshan@users.noreply.github.com>
2022-11-29 09:53:08 +0530

    Add files via upload

---
+++ Binding Event Handlers/Used arrow function in call method/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+import One  from './Components/Oneatribute'
+// import Towl from './Components/Twoatribute'
+ import Prop from './Components/Properties'
+  import Getclass from './Classcomponent/Getclass'
+
+
+function App() {
+  return (
+    <div className="App">
+      <Getclass/>
+
+
+    </div>
+  );
+}
+
+export default App;
