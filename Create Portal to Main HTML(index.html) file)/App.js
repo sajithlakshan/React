13e5d7d88034aedebd62aeb0b01d6aed39b47cfe
sajithlakshan <57319180+sajithlakshan@users.noreply.github.com>
2022-12-02 10:31:44 +0530

    Add files via upload

---
+++ Create Portal to Main HTML(index.html) file)/App.js
@@ -0,0 +1,22 @@
+
+import './App.css';
+import FRParentinput from './Forwarding Refs/FRParentinput';
+import Mypotral from './Potral/potral';
+
+import Mrefdemo from './Ref/refdemo';
+import Focuseinput from './Refs with Class Components/Focuseinput';
+
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Mrefdemo></Mrefdemo> */}
+   {/* <Focuseinput/> */}
+   {/* <FRParentinput/> */}
+    <Mypotral/>
+    
+    </div>
+  );
+}
+
+export default App;
