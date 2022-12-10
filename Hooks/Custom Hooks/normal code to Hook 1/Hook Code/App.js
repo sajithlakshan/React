5a76329b7be3401d12113014f8ff04c0ecf664f2
sajithlakshan <57319180+sajithlakshan@users.noreply.github.com>
2022-12-10 16:16:41 +0530

    Add files via upload

---
+++ Hooks/Custom Hooks/normal code to Hook 1/Hook Code/App.js
@@ -0,0 +1,20 @@
+
+import React from 'react';
+import './App.css';
+import  UsedCustomHooks from './Custom Hooks/MyCustomHooks';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+
+       < UsedCustomHooks/>
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ Hooks/Custom Hooks/normal code to Hook 1/Hook Code/MyCustomHooks.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import useFetch from './useFetch';
+
+const UsedCustomHooks = () => {
+    const data = useFetch("https://jsonplaceholder.typicode.com/users")
+
+    
+  return (
+    <div>
+        {
+            data.map((res)=>{
+                return(
+                    <h4 key={res.id}>
+                        {res.id}.{res.name}
+
+                    </h4>
+                )
+            })
+        }
+    </div>
+  )
+}
+
+export default UsedCustomHooks
